fa936a5e321c5fba3fee6422958405cde22bcc98
Nastassia Kamiakova <nastassia294@gmail.com>
2021-03-11 20:25:16 +0300

    Exercises 35 - 38

---
+++ ex38_2.py
@@ -0,0 +1,19 @@
+#ex38
+
+ten_things = "Apple Oranges Crows Telephone Light Sugar"
+
+stuff = ten_things.split(' ')
+print ("Now array consists of ", ' '.join(stuff))
+
+print (f"Array has {len(stuff)} items\n")
+
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
+
+for asdfg in stuff:
+    next_one = more_stuff.pop()
+    stuff.append(next_one)
+    if len(stuff) == 11:
+        break
+    print ("element ", next_one, " is added to the array")
+    print (f"Now the array has {len(stuff)} items")
+    print (stuff, "\n")
