0777a08c736a8690ff96cbae2cefdcd5e9240f2c
Nastassia Kamiakova <nastassia294@gmail.com>
2021-03-20 18:23:36 +0300

    Max/min values and indexes

--- experiments/sort.py
+++ experiments/sort.py
@@ -1,10 +1,18 @@
-def sort(array):
+# Sorts the given list of elements
+# in the increasing order.
+def sort(list):
   # TODO Implement me!
-  sorted_array = sorted (array)
+  # loops
+  # ifs/conditions
+  # comparisons of list elements
+  # swapss
+  # list[1] < list[2]
+  # list[1] = list[2]
+  sorted_array = sorted (list)
   return sorted_array
 
-print(sort([]))
-print(sort([1]))
-print(sort([2, 1]))
-print(sort([0, 5, 7, 8, 6, 3, 4, 1, 9, 2]))
-print(sort(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']))
+#print(sort([]))
+#print(sort([1]))
+#print(sort([2, 1]))
+#print(sort([0, 5, 7, 8, 6, 3, 4, 1, 9, 2]))
+#print(sort(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']))
