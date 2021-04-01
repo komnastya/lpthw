492f2643ed4e340749809ca7179f0867bcdbec2b
Nastassia Kamiakova <nastassia294@gmail.com>
2021-04-01 15:27:14 +0300

    Implemented sort function

--- experiments/sort.py
+++ experiments/sort.py
@@ -1,6 +1,5 @@
 # Sorts the given list of elements
 # in the increasing order.
-def sort(list):
   # TODO Implement me!
   # loops
   # ifs/conditions
@@ -8,11 +7,17 @@ def sort(list):
   # swapss
   # list[1] < list[2]
   # list[1] = list[2]
-  sorted_array = sorted (list)
-  return sorted_array
 
-#print(sort([]))
-#print(sort([1]))
-#print(sort([2, 1]))
-#print(sort([0, 5, 7, 8, 6, 3, 4, 1, 9, 2]))
-#print(sort(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']))
+def is_sorted(list):
+  if len(list) == 0:
+    return False
+  for x in range (len(list) - 1):
+    if list[x] > list[x + 1]:
+      return False
+  return True
+
+print(is_sorted([]))
+print(is_sorted([1]))
+print(is_sorted([2, 1]))
+print(is_sorted([0, 5, 7, 8, 6, 3, 4, 1, 9, 2]))
+print(is_sorted([1,2,3,4,5,6,7]))
