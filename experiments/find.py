fa2bf43b624c129f61677517f1730068cfdeb957
Nastassia Kamiakova <nastassia294@gmail.com>
2021-04-07 17:39:09 +0300

    New find() function with unit test

--- experiments/find.py
+++ experiments/find.py
@@ -9,15 +9,7 @@
 # Given list=[0,2,2,4], element=1, result=-1
 
 def find(list, element):
-  index = -1
   for i in range (len(list)):
     if list[i] == element:
-      index = i
-  return index
-
-print ('0 =', find ([1,2,3,4,5,6,7], 1))
-print ('2 =', find ([1,2,3,4,5,6,7], 3))
-print ('4 =', find ([1,2,3,4,5,6,7], 5))
-print ('-1 =', find ([1,2,3,4,5,6,7], 14))
-print ('-1 =', find ([1,2,3,4,5,6,7], 0))
-print ('-1 =', find ([1,2,3,4,5,6,7], 9))
+      return i
+  return -1
---
+++ experiments/find_tests.py
@@ -0,0 +1,17 @@
+# python -m unittest discover -p "*_test.py"
+
+import unittest
+from find import find
+
+class TestFind(unittest.TestCase):
+
+    def test_find(self):
+      self.assertEqual(find([], 1), -1)
+      self.assertEqual(find([1,1,1,1,1], 1), 0)
+      self.assertEqual(find([1,2,3,4,5], 1), 0)
+      self.assertEqual(find([1,2,3,4,5], 3), 2)
+      self.assertEqual(find([1,2,3,4,5], 5), 4)
+      self.assertEqual(find([1,2,3,4,5], 0), -1)
+
+if __name__ == '__main__':
+    unittest.main()
