e66d3965627833996ac28fe21375d55bdc9ea644
Nastassia Kamiakova <nastassia294@gmail.com>
2021-04-08 19:38:56 +0300

    shift() with unit tests

---
+++ experiments/shift_tests.py
@@ -0,0 +1,19 @@
+# python -m unittest discover -p "*_test.py"
+
+import unittest
+from shift import shift_while, shift_for
+
+class TestShift(unittest.TestCase):
+
+    def test_shift_while(self):
+        self.assertEqual(shift_while([]), [])
+        self.assertEqual(shift_while([1]), [1])
+        self.assertEqual(shift_while([1,2,3,4,5]), [2,3,4,5,1])
+
+    def test_shift_for(self):
+        self.assertEqual(shift_for([]), [])
+        self.assertEqual(shift_for([1]), [1])
+        self.assertEqual(shift_for([1,2,3,4,5]), [2,3,4,5,1])
+
+if __name__ == '__main__':
+    unittest.main()
