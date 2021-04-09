2550d57ef341847421181b8337476d3fbeb2ab7e
Nastassia Kamiakova <nastassia294@gmail.com>
2021-04-09 21:47:51 +0300

    truncate() with unit test

---
+++ experiments/truncate.py
@@ -0,0 +1,40 @@
+#- Ensures that the given list is not longer than max_length:
+
+# Example: truncate([], 3) -> []
+# Example: truncate([1,2,3,4,5,6], 3) -> [1,2,3]
+# Example: truncate([1,2,3,4,5,6], 0) -> []
+# Example: truncate([1,2,3,4,5,6], 1000) ->
+
+
+  # TODO Implement me!
+  # Write two implementations:
+  # 1. An implementation which returns a new list.
+  # 2. An implementation which modifies the given list and does not return anything.
+
+# first variant
+
+def truncate(list, max_length):
+    output = []
+    if len(list) == 0:
+        return output
+    # elif len(list) < max_length:
+     # return list # FIXME: неправильно, надо вернуть новый список, а не list
+    # i = 0
+    # while len(output) < max_length:
+    #   output.append(list[i])
+      #  i = i + 1
+    for i in range (len(list)):
+      if i < max_length:
+        output.append(list[i])
+    return output
+
+# second variant
+
+def truncate_2(list, max_length):
+  del list[max_length:]
+
+def truncate_3(list, max_length):
+  if len(list) == 0:
+    return list
+  while len(list) > max_length:
+   list.pop() # OR del list[-1]
---
+++ experiments/truncate_tests.py
@@ -0,0 +1,13 @@
+import unittest
+from truncate import truncate
+
+class TestFind(unittest.TestCase):
+
+    def test_find(self):
+      self.assertEqual(truncate([],3), [])
+      self.assertEqual(truncate([1, 2, 3, 4, 5, 6], 3), [1,2,3])
+      self.assertEqual(truncate([1, 2, 3, 4, 5, 6], 0), [])
+      self.assertEqual(truncate([1,2,3], 1_000_000), [1,2,3])
+
+if __name__ == '__main__':
+    unittest.main()
