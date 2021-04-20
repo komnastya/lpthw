1beeca5ea679a80900f97ac8f4c1017dd93ec51d
Nastassia Kamiakova <nastassia294@gmail.com>
2021-04-20 18:50:50 +0300

    delete unnecessary comments

--- tic-tac-toe game/tictactoeviaconsole.py
+++ tic-tac-toe game/tictactoeviaconsole.py
@@ -5,49 +5,29 @@ class InvalidMoveError(Exception):
     pass
 
 
-#def gameover(board, why):
- #   show(board, why) # you already call the show function after each move,
-                     # so you can simplify by altering the game function.
-                     # print this text in the game function instead.
-  #  print('Game is over!')
-    # FIXME This is a valid strategy to exit a programm,
-    # but there are better alternatives.
-    # Try to fix your code in such a way that the `game` function
-    # simply returns when a game is over.
-    # exit(0)
-
-def winner(v, h, moves):
+def winner(moves):
     board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-    while len(moves) < 9:
-        for istep, (v, h) in enumerate(moves):
-            if board[v][h] != " ":
-                raise InvalidMoveError()
-            mark = None
-            if istep % 2 == 0:
-                mark = 'X'
-            else:
-                mark = 'O'
-            board[v][h] = mark
-            if (
-                    # check row v
-                    board[v][0] == board[v][1] == board[v][2] != " "
-                    # check col h
-                    or board[0][h] == board[1][h] == board[2][h] != " "
-                    # check diagonal 1
-                    or board[0][0] == board[1][1] == board[2][2] != " "
-                    # check diagonal 2
-                    or board[2][0] == board[1][1] == board[0][2] != " "):
-                if mark == 'X':
-                    return board, 'A is winner'
-                if mark == 'O':
-                    return board, 'B is winner'
-        return board, 'Pending...\n'
+    for istep, (v, h) in enumerate(moves):
+        if board[v][h] != " ":
+            raise InvalidMoveError()
+        mark = None
+        if istep % 2 == 0:
+            mark = 'X'
+        else:
+            mark = 'O'
+        board[v][h] = mark
+        if (
+                board[v][0] == board[v][1] == board[v][2] != " "
+                or board[0][h] == board[1][h] == board[2][h] != " "
+                or board[0][0] == board[1][1] == board[2][2] != " "
+                or board[2][0] == board[1][1] == board[0][2] != " "):
+            if mark == 'X':
+                return board, 'A is winner'
+            if mark == 'O':
+                return board, 'B is winner'
     if len(moves) == 9:
-        # FIXME Again, the purpose of this function is to simply determine
-        # the current winner. By terminating the program it takes on itself
-        # too many responsibilities.
-        # FIXME No need for the `return` keyword. The gameover function never returns!
         return board, 'Draw'
+    return board, 'Pending...'
 
 
 def show(board, output):
@@ -64,19 +44,16 @@ def show(board, output):
 def game():
     moves = []
     while True:
-        # To improve the user experience we can input both v and h in a single string:
-        move = input('Chose your move: ').strip()
+        move = input('\nChose your move: ').strip()
         v, h = move.split(" ")
         v, h = int(v) - 1, int(h) - 1
         if 0 <= v < 4 and 0 <= h < 4:
             moves.append((v, h))
-            board, output = winner(v, h, moves)
+            board, output = winner(moves)
             show(board, output)
             if output == 'A is winner' or output == 'B is winner' or output == 'Draw':
                 print('\nGame is over!')
                 break
-            # TODO Determine if the game is over, show a message and break.
-            # ok
         else:
             # FIXME You are overeacting here.
             # It is the responsibility of the winner function to raise exceptions.
@@ -86,4 +63,4 @@ def game():
 
 print("Welcome to the Tic Tac Toe game!\n")
 
-game()  # Start the game.
+game()
