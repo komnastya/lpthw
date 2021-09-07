a82bfa23ca6716b341e0a4a5257f8631f28040d8
Nastassia Kamiakova <nastassia294@gmail.com>
2021-09-07 18:25:40 +0300

    Improve tests for matrix functions

--- experiments/matrix_tests.py
+++ experiments/matrix_tests.py
@@ -8,14 +8,16 @@ class TestMatrix(unittest.TestCase):
         self.assertTrue(is_square([]))
         self.assertTrue(is_square([[1]]))
         self.assertTrue(is_square([[1, 2], [3, 4]]))
-        # TODO self.assertFalse(is_square([[1],[2]]))
+        with self.assertRaises(ValueError):
+            matrix_sum([[1, 2], [3, 4]], [[1, 2]])
 
     def test_matrix_sum(self):
         self.assertEqual(matrix_sum([], []), [])
         self.assertEqual(
             matrix_sum([[1, 2], [3, 4]], [[5, 6], [7, 8]]), [[6, 8], [10, 12]]
         )
-        # TODO self.assertFalse(matrix_sum([[1,2], [3,4]], [[1,2]]))
+        with self.assertRaises(ValueError):
+            matrix_sum([[1, 2], [3, 4]], [[1, 2]])
 
 
 if __name__ == "__main__":
