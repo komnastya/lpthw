39004a22fb1a92861ea43328c9dc53b9229e8d55
Nastassia Kamiakova <nastassia294@gmail.com>
2021-09-07 20:23:53 +0300

    Add the tests for the trunctate 2/3 functions

--- experiments/truncate_tests.py
+++ experiments/truncate_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from truncate import truncate
+from truncate import truncate, truncate_2, truncate_3
 
 
 class TestTruncate(unittest.TestCase):
@@ -10,6 +10,39 @@ class TestTruncate(unittest.TestCase):
         self.assertEqual(truncate([1, 2, 3, 4, 5, 6], 0), [])
         self.assertEqual(truncate([1, 2, 3], 1_000_000), [1, 2, 3])
 
+    def test_truncate_2(self):
+        nums = [1, 2, 3, 4, 5, 6]
+        truncate_2(nums, 3)
+        self.assertEqual(nums, [1, 2, 3])
+
+        nums = [1, 2, 3, 4, 5, 6]
+        truncate_2(nums, 0)
+        self.assertEqual(nums, [])
+
+        nums = [1, 2, 3, 4, 5, 6]
+        truncate_2(nums, 1000)
+        self.assertEqual(nums, [1, 2, 3, 4, 5, 6])
+
+        nums = []
+        truncate_2(nums, 1000)
+        self.assertEqual(nums, [])
+
+    def test_truncate_3(self):
+        nums = [1, 2, 3, 4, 5, 6]
+        truncate_3(nums, 3)
+        self.assertEqual(nums, [1, 2, 3])
+
+        nums = [1, 2, 3, 4, 5, 6]
+        truncate_3(nums, 0)
+        self.assertEqual(nums, [])
+
+        nums = [1, 2, 3, 4, 5, 6]
+        truncate_3(nums, 1000)
+        self.assertEqual(nums, [1, 2, 3, 4, 5, 6])
+
+        nums = []
+        truncate_3(nums, 1000)
+        self.assertEqual(nums, [])
 
 if __name__ == "__main__":
     unittest.main()
